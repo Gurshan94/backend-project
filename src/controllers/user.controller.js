ea3defd8dec31d560e096a4ff510164a96998e24
Gurshan94 <gurshan94u@gmail.com>
2024-08-30 22:36:06 +0530

    fixed typos

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -245,7 +245,11 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async (req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"current user feteched successfully")
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "user fetched successfully"
+    ))
 })
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
@@ -254,7 +258,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
         throw new ApiError(400, "all fields are required")
     }
 
-    const user = User.findByIdAndUpdate(
+    const user =await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set :{
